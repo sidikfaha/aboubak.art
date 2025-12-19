a727c07275946f0ccabbde90b555c59ebc74c78b
sidikfaha <me@aboubak.art>
2025-12-19 16:24:38 +0000

    feat: add @nuxtjs/sitemap module and configure site URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/icon", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/i18n",
+    "@nuxtjs/sitemap",
+  ],
+  site: {
+    url: "https://aboubak.art",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
