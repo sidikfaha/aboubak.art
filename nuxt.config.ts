fdca2e1ee9a879fc93820c8b9d0bc12c007d0e41
sidikfaha <joefaha1@gmail.com>
2026-02-05 17:46:46 +0000

    chore(v2): initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/icon"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/tailwind.css"],
+});
